1f5511f7ccebdc45c2878687a31a5b93874a690f
Akash <thakurakash318@gmail.com>
2025-03-04 14:14:56 +0530

    created magic table for slider pieces attacks

--- AI_POWERED_CHESS/main.cpp
+++ AI_POWERED_CHESS/main.cpp
@@ -5,6 +5,7 @@
 //  Created by AKASH THAKUR on 02/03/25.
 //
 #include <iostream>
+#include<string.h>
 //#include<GL/glew.h>
 //#include<SFML/Window.hpp>
 //#include<SFML/Graphics.hpp>
@@ -24,10 +25,21 @@ enum {
     a2, b2, c2, d2, e2, f2, g2, h2,
     a1, b1, c1, d1, e1, f1, g1, h1
 };
-//sides to move (colors) (white = 0, black = 1)
-enum {
-    white,black
+
+const char *square_to_coordinate[64] = {
+   "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+   "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+   "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+   "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+   "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+   "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+   "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+   "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
 };
+//sides to move (colors) (white = 0, black = 1)
+enum { white , black };
+//biahop and rook
+enum{ rook , bishop };
 
 //for future
 /*
@@ -41,16 +53,16 @@ enum {
 1-> 56 57 58 59 60 61 62 63
      a  b  c  d  e  f  g   h
  
- 
-"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
-"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
-"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
-"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
-"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
-"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+
 */
+
+/*** * * *  * * * * * * * * * * * * * ** * **\
+ ==================================
+ 
+      BITWISE MANIPULATIONS
+ 
+ ==================================
+\ * ** * * ** ** * * ** * ** * ** ** * * ** */
 //set/get.pop macro
 #define get_bit(bitboard,square) (bitboard & (1ULL)<<square)
 #define set_bit(bitboard,square) (bitboard |= (1ULL)<<square)
@@ -59,7 +71,7 @@ enum {
 
 // function to count number of bits in the U64
 
-static inline int count_bit(U64 bitboard){
+static inline int count_bits(U64 bitboard){
     //init count as 0
     int count = 0;
     
@@ -76,6 +88,18 @@ static inline int count_bit(U64 bitboard){
     return count;
 }
 
+// fucntion to find the least significant bit position
+
+static inline int get_ls1b_index(U64 bitboard){
+    //make sure the bitboard is not 0
+    if(bitboard){
+        return count_bits
+        
+        ((bitboard & -bitboard)-1);
+    }
+    else
+        return -1 ;
+}
 //function to print bitboard
 void print_bitboard(U64 bitboard){
     cout<<endl;
@@ -94,6 +118,54 @@ void print_bitboard(U64 bitboard){
     //print the bitboard in decimal form
     printf("   Bitboard: %llud \n",bitboard);
     
+}
+
+/*** * * *  * * * * * * * * * * * * * ** * **\
+ ==================================
+ 
+            RANDOM NUMBERS
+ 
+ ==================================
+\ * ** * * ** ** * * ** * ** * ** ** * * ** */
+
+
+
+//pseudo  random number state
+unsigned int random_state = 1804289383 ;
+
+//generate 32-bit pseudo legal numbers
+unsigned int get_random_U32_number(){
+    //get current state
+    unsigned int number = random_state ;
+    
+    //xor shift algorithm
+    number ^= number<<13;
+    number ^= number>>17 ;
+    number ^= number<<5;
+    
+    //update random number state
+    random_state = number ;
+    //return random number ;
+    return number ;
+}
+//generate 64-bit pseudo legal numbers
+U64 get_random_U64_number(){
+    //define number
+    U64 n1,n2,n3,n4;
+    
+    //init random numbers slicing 16 bits from the MSB side
+    n1 = (U64)(get_random_U32_number()) & 0xFFFF ;
+    n2 = (U64)(get_random_U32_number()) & 0xFFFF ;
+    n3 = (U64)(get_random_U32_number()) & 0xFFFF ;
+    n4 = (U64)(get_random_U32_number()) & 0xFFFF ;
+    
+    //return random number
+    return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48) ;
+}
+
+//generate magic number candidate
+U64 generate_magic_number(){
+    return get_random_U64_number() & get_random_U64_number() & get_random_U64_number() ;
 }
 
 
@@ -176,6 +248,165 @@ const U64 not_hg_file = 4557430888798830399ULL;
 // not AB file constant
 const U64 not_ab_file = 18229723555195321596ULL;
 
+//bishop relavant occupancy bit count for every square on chess board
+const int bishop_relevant_bits[64] ={
+    6, 5, 5, 5, 5, 5, 5, 6,
+    5, 5, 5, 5, 5, 5, 5, 5,
+    5, 5, 7, 7, 7, 7, 5, 5,
+    5, 5, 7, 9, 9, 7, 5, 5,
+    5, 5, 7, 9, 9, 7, 5, 5,
+    5, 5, 7, 7, 7, 7, 5, 5,
+    5, 5, 5, 5, 5, 5, 5, 5,
+    6, 5, 5, 5, 5, 5, 5, 6
+};
+//rook relavant occupancy bit count for every square on chess board
+const int rook_relevant_bits[64] ={
+    12, 11, 11, 11, 11, 11, 11, 12,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    12, 11, 11, 11, 11, 11, 11, 12
+};
+
+
+// rook magic numbers
+U64 rook_magic_numbers[64] = {
+    0x8a80104000800020ULL,
+    0x140002000100040ULL,
+    0x2801880a0017001ULL,
+    0x100081001000420ULL,
+    0x200020010080420ULL,
+    0x3001c0002010008ULL,
+    0x8480008002000100ULL,
+    0x2080088004402900ULL,
+    0x800098204000ULL,
+    0x2024401000200040ULL,
+    0x100802000801000ULL,
+    0x120800800801000ULL,
+    0x208808088000400ULL,
+    0x2802200800400ULL,
+    0x2200800100020080ULL,
+    0x801000060821100ULL,
+    0x80044006422000ULL,
+    0x100808020004000ULL,
+    0x12108a0010204200ULL,
+    0x140848010000802ULL,
+    0x481828014002800ULL,
+    0x8094004002004100ULL,
+    0x4010040010010802ULL,
+    0x20008806104ULL,
+    0x100400080208000ULL,
+    0x2040002120081000ULL,
+    0x21200680100081ULL,
+    0x20100080080080ULL,
+    0x2000a00200410ULL,
+    0x20080800400ULL,
+    0x80088400100102ULL,
+    0x80004600042881ULL,
+    0x4040008040800020ULL,
+    0x440003000200801ULL,
+    0x4200011004500ULL,
+    0x188020010100100ULL,
+    0x14800401802800ULL,
+    0x2080040080800200ULL,
+    0x124080204001001ULL,
+    0x200046502000484ULL,
+    0x480400080088020ULL,
+    0x1000422010034000ULL,
+    0x30200100110040ULL,
+    0x100021010009ULL,
+    0x2002080100110004ULL,
+    0x202008004008002ULL,
+    0x20020004010100ULL,
+    0x2048440040820001ULL,
+    0x101002200408200ULL,
+    0x40802000401080ULL,
+    0x4008142004410100ULL,
+    0x2060820c0120200ULL,
+    0x1001004080100ULL,
+    0x20c020080040080ULL,
+    0x2935610830022400ULL,
+    0x44440041009200ULL,
+    0x280001040802101ULL,
+    0x2100190040002085ULL,
+    0x80c0084100102001ULL,
+    0x4024081001000421ULL,
+    0x20030a0244872ULL,
+    0x12001008414402ULL,
+    0x2006104900a0804ULL,
+    0x1004081002402ULL
+};
+
+// bishop magic numbers
+U64 bishop_magic_numbers[64] = {
+    0x40040844404084ULL,
+    0x2004208a004208ULL,
+    0x10190041080202ULL,
+    0x108060845042010ULL,
+    0x581104180800210ULL,
+    0x2112080446200010ULL,
+    0x1080820820060210ULL,
+    0x3c0808410220200ULL,
+    0x4050404440404ULL,
+    0x21001420088ULL,
+    0x24d0080801082102ULL,
+    0x1020a0a020400ULL,
+    0x40308200402ULL,
+    0x4011002100800ULL,
+    0x401484104104005ULL,
+    0x801010402020200ULL,
+    0x400210c3880100ULL,
+    0x404022024108200ULL,
+    0x810018200204102ULL,
+    0x4002801a02003ULL,
+    0x85040820080400ULL,
+    0x810102c808880400ULL,
+    0xe900410884800ULL,
+    0x8002020480840102ULL,
+    0x220200865090201ULL,
+    0x2010100a02021202ULL,
+    0x152048408022401ULL,
+    0x20080002081110ULL,
+    0x4001001021004000ULL,
+    0x800040400a011002ULL,
+    0xe4004081011002ULL,
+    0x1c004001012080ULL,
+    0x8004200962a00220ULL,
+    0x8422100208500202ULL,
+    0x2000402200300c08ULL,
+    0x8646020080080080ULL,
+    0x80020a0200100808ULL,
+    0x2010004880111000ULL,
+    0x623000a080011400ULL,
+    0x42008c0340209202ULL,
+    0x209188240001000ULL,
+    0x400408a884001800ULL,
+    0x110400a6080400ULL,
+    0x1840060a44020800ULL,
+    0x90080104000041ULL,
+    0x201011000808101ULL,
+    0x1a2208080504f080ULL,
+    0x8012020600211212ULL,
+    0x500861011240000ULL,
+    0x180806108200800ULL,
+    0x4000020e01040044ULL,
+    0x300000261044000aULL,
+    0x802241102020002ULL,
+    0x20906061210001ULL,
+    0x5a84841004010310ULL,
+    0x4010801011c04ULL,
+    0xa010109502200ULL,
+    0x4a02012000ULL,
+    0x500201010098b028ULL,
+    0x8040002811040900ULL,
+    0x28000010020204ULL,
+    0x6000020202d0240ULL,
+    0x8918844842082200ULL,
+    0x4010011029020020ULL
+};
 // pawn attacks table [side][square] (sipahi = pawn)
 U64 pawn_attacks[2][64];
 
@@ -420,6 +651,151 @@ void init_leapers_attacks()
     }
 }
 
+// set occupancies
+U64 set_occupancy(int index, int bits_in_mask, U64 attack_mask)
+{
+    // occupancy map
+    U64 occupancy = 0ULL;
+    
+    // loop over the range of bits within attack mask
+    for (int count = 0; count < bits_in_mask; count++)
+    {
+        // get LS1B index of attacks mask
+        int square = get_ls1b_index(attack_mask);
+        
+        // pop LS1B in attack map
+        pop_bit(attack_mask, square);
+        
+        // make sure occupancy is on board
+        if (index & (1 << count))
+            // populate occupancy map
+            occupancy |= (1ULL << square);
+    }
+    
+    // return occupancy map
+    return occupancy;
+}
+
+/*** * * *  * * * * * * * * * * * * * ** * **\
+ ==================================
+ 
+      MAGICS
+ 
+ ==================================
+\ * ** * * ** ** * * ** * ** * ** ** * * ** */
+
+// find appropriate magic number
+U64 find_magic_number(int square, int relevant_bits, int bishop)
+{
+    // init occupancies
+    U64 occupancies[4096];
+    
+    // init attack tables
+    U64 attacks[4096];
+    
+    // init used attacks
+    U64 used_attacks[4096];
+    
+    // init attack mask for a current piece
+    U64 attack_mask = bishop ? mask_bishop_attacks(square) : mask_rook_attacks(square);
+    
+    // init occupancy indicies
+    int occupancy_indicies = 1 << relevant_bits;
+    
+    // loop over occupancy indicies
+    for (int index = 0; index < occupancy_indicies; index++)
+    {
+        // init occupancies
+        occupancies[index] = set_occupancy(index, relevant_bits, attack_mask);
+        
+        // init attacks
+        attacks[index] = bishop ? bishop_attacks_on_the_fly(square, occupancies[index]) :
+                                    rook_attacks_on_the_fly(square, occupancies[index]);
+    }
+    
+    // test magic numbers loop
+    for (int random_count = 0; random_count < 100000000; random_count++)
+    {
+        // generate magic number candidate
+        U64 magic_number = generate_magic_number();
+        
+        // skip inappropriate magic numbers
+        if (count_bits((attack_mask * magic_number) & 0xFF00000000000000) < 6) continue;
+        
+        // init used attacks
+        memset(used_attacks, 0ULL, sizeof(used_attacks));
+        
+        // init index & fail flag
+        int index, fail;
+        
+        // test magic index loop
+        for (index = 0, fail = 0; !fail && index < occupancy_indicies; index++)
+        {
+            // init magic index
+            int magic_index = (int)((occupancies[index] * magic_number) >> (64 - relevant_bits));
+            
+            // if magic index works
+            if (used_attacks[magic_index] == 0ULL)
+                // init used attacks
+                used_attacks[magic_index] = attacks[index];
+            
+            // otherwise
+            else if (used_attacks[magic_index] != attacks[index])
+                // magic index doesn't work
+                fail = 1;
+        }
+        
+        // if magic number works
+        if (!fail)
+            // return it
+            return magic_number;
+    }
+    
+    // if magic number doesn't work
+    printf("  Magic number fails!\n");
+    return 0ULL;
+}
+
+// init magic numbers
+void init_magic_numbers()
+{
+    // loop over 64 board squares
+    for (int square = 0; square < 64; square++)
+        // init rook magic numbers
+        rook_magic_numbers[square] = find_magic_number(square, rook_relevant_bits[square], rook);
+
+    // loop over 64 board squares
+    for (int square = 0; square < 64; square++)
+        // init bishop magic numbers
+        bishop_magic_numbers[square] = find_magic_number(square, bishop_relevant_bits[square], bishop);
+}
+
+
+/*** * * *  * * * * * * * * * * * * * ** * **\
+ ==================================
+ 
+      INIT ALL
+ 
+ ==================================
+\ * ** * * ** ** * * ** * ** * ** ** * * ** */
+
+// init all variables
+void init_all()
+{
+    // init leaper pieces attacks
+    init_leapers_attacks();
+    
+    // init magic numbers
+    //init_magic_numbers();
+}
+
+/*** * * *  * * * * * * * * * * * * * ** * **\
+ ==================================
+ 
+              DRIVER MAIN FUNCTION
+ 
+ ==================================
+\ * ** * * ** ** * * ** * ** * ** ** * * ** */
 int main() {
    /* define bitboard
     U64 bitboard = 0ULL;
@@ -436,17 +812,8 @@ int main() {
     print_bitboard(bitboard);
   */
     // init leaper pieces attacks
-    init_leapers_attacks();
-    
-    // loop over 64 board squares
-    U64 block = 0ULL;
-    set_bit(block, d6);
-    set_bit(block, b4);
-    set_bit(block, d2);
-    set_bit(block, g4);
-    print_bitboard(block);
-    cout<<count_bit(block);
-
+    init_all();
+    //print_bitboard((U64)get_random_U32_number());
     return 0;
 }
 
